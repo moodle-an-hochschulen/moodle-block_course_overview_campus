8e5a67c486f511ea4a073b27c895c730fbe3122c
Kathrin <kathrin.osswald@uni-ulm.de>
2016-05-17 15:06:18 +0200

    Fix to issue LMS-882: Verify active teacher enrolment
    Verify active teacher enrolment

--- block_course_overview_campus.php
+++ block_course_overview_campus.php
@@ -463,20 +463,40 @@ class block_course_overview_campus extends block_base {
 
                 // Teacher information
                 if ($coc_config->teachercoursefilter == true || $coc_config->secondrowshowteachername == true) {
+
+                    $now1 = round(time(), -2); // improves db caching
+                    $now2 = $now1;
+                    $statussuspended = 0;
+                    $enrolmentactive = 0;
+
+                    $extrawhere = 'ue.status = '.$statussuspended.' AND e.status = '.$enrolmentactive.' AND ue.timestart < '.$now1.' AND (ue.timeend = 0 OR ue.timeend > '.$now2.')';
+
                     // Get course teachers based on global teacher roles
                     if (count($teacherroles) > 0) {
                         // Check if we have to include teacher roles from parent contexts
                         // If yes
                         if ($coc_config->teacherrolesparent == 1) {
-                            $courseteachers = get_role_users($teacherroles, $context, true, 'ra.id, u.id, u.lastname, u.firstname, r.sortorder', 'u.lastname, u.firstname');
+                            if ($coc_config->teacherroleshidesuspendedteacher == 0) { // also show suspended teachers
+                                $courseteachers = get_role_users($teacherroles, $context, true, 'ra.id, u.id, u.lastname, u.firstname, r.sortorder', 'u.lastname, u.firstname');
+                            } else { // hide suspended teachers
+                                $courseteachers = get_role_users($teacherroles, $context, true, 'ra.id, u.id, u.lastname, u.firstname, r.sortorder', 'u.lastname, u.firstname', false, '', '', '', $extrawhere);
+                            }
                         }
                         // If no
                         else if ($coc_config->teacherrolesparent == 2) {
-                            $courseteachers = get_role_users($teacherroles, $context, false, 'ra.id, u.id, u.lastname, u.firstname, r.sortorder', 'u.lastname, u.firstname');
+                            if ($coc_config->teacherroleshidesuspendedteacher == 0) { // also show suspended teachers
+                                $courseteachers = get_role_users($teacherroles, $context, false, 'ra.id, u.id, u.lastname, u.firstname, r.sortorder', 'u.lastname, u.firstname');
+                            } else { // hide suspended teachers
+                                $courseteachers = get_role_users($teacherroles, $context, false, 'ra.id, u.id, u.lastname, u.firstname, r.sortorder', 'u.lastname, u.firstname', false, '', '', '', $extrawhere);
+                            }
                         }
                         // If depending on moodle/course:reviewotherusers capability
                         else if ($coc_config->teacherrolesparent == 3) {
-                            $courseteachers = get_role_users($teacherroles, $context, has_capability('moodle/course:reviewotherusers', $context), 'ra.id, u.id, u.lastname, u.firstname, r.sortorder', 'u.lastname, u.firstname');
+                            if ($coc_config->teacherroleshidesuspendedteacher == 0) { // also show suspended teachers
+                                $courseteachers = get_role_users($teacherroles, $context, has_capability('moodle/course:reviewotherusers', $context), 'ra.id, u.id, u.lastname, u.firstname, r.sortorder', 'u.lastname, u.firstname');
+                            } else { // hide suspended teachers
+                                $courseteachers = get_role_users($teacherroles, $context, has_capability('moodle/course:reviewotherusers', $context), 'ra.id, u.id, u.lastname, u.firstname, r.sortorder', 'u.lastname, u.firstname', false, '', '', '', $extrawhere);
+                            }
                         }
                         // Should not happen
                         else {
